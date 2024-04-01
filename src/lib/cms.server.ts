f5000d8637a925d6c6ba073f2ed932d4d83fd4a5
apple-phi <lkn849@gmail.com>
2024-04-01 21:08:47 +0100

    Abstract out cms types to be importable on the frontend

--- src/lib/cms.server.ts
+++ src/lib/cms.server.ts
@@ -1,69 +1,12 @@
 import { CMS_AUTH, CMS_GETBYPATH, CMS_HOST, CMS_MAIN_COLLECTION } from '$env/static/private';
+import type {
+	CommitteePage,
+	CommitteePageQueryResponse,
+	PageResponse,
+	ReturnNavHeader
+} from './cms.types';
 const headers = { Authorization: CMS_AUTH };
 const cmsApiUrl = 'https://' + CMS_HOST + '/api/';
-
-export interface CommitteePage {
-	id: string;
-	approved: boolean;
-	readyforapproval: boolean;
-	lastEditedBy: {
-		id: string;
-		committee_information: object;
-		email: string;
-		createdAt: string;
-		updatedAt: string;
-	};
-	title: string;
-	path: string;
-	content: object;
-	category: string;
-	createdAt: string;
-	updatedAt: string;
-	approveditems: {
-		path: string;
-		title: string;
-		lastEditedBy: string;
-		lastEditedByOfficeText: string;
-		lastEditedAt: string;
-		acontent: object;
-		content_html: string;
-	};
-}
-
-export interface CommitteePageQueryResponse {
-	docs: CommitteePage[];
-	totalDocs: number;
-	limit: number;
-	totalPages: number;
-	page: number;
-	pagingCounter: number;
-	hasPrevPage: boolean;
-	hasNextPage: boolean;
-	prevPage?: number | null;
-	nextPage?: number | null;
-}
-
-export interface ReturnNavItem {
-	label: string;
-	path: string;
-}
-
-export interface ReturnNavHeader {
-	header: string;
-	navitems?: ReturnNavItem[];
-}
-
-export interface PageResponse {
-	path: string;
-	title: string;
-	html: string;
-	lastEditedBy: string;
-	lastEditedAt: string;
-}
-
-export type GetPages = PageResponse[];
-
-export type ReturnNavBar = ReturnNavHeader[];
 
 /**
  * Fetches page content from a specified path.
