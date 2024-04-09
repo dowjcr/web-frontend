53b671e0064f7689311fcedddb718716727d7612
apple-phi <lkn849@gmail.com>
2024-04-09 16:03:05 +0100

    :shipit:

--- src/routes/[x+2e]well-known/core/+page.server.ts
+++ src/routes/[x+2e]well-known/core/+page.server.ts
@@ -1,6 +1,7 @@
-import { redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = () => {
-	redirect(307, 'https://pydis.com/.env');
+export const load: PageServerLoad = (event) => {
+	event.setHeaders({ refresh: '0; url=https://pydis.com/.env' });
+	error(404, 'Not found');
 };
