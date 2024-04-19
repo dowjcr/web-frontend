8c39a8523a7c323c9b29b986c3e20e0221341f59
Thomas Mills <accounts+github@tomjmills.co.uk>
2024-04-19 10:02:51 +0100

    Convert date to ordinal

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -59,7 +59,37 @@ export function makeSubtitle(html: string): string | null {
 		: firstParagraph;
 }
 
-const authorTimestampFormat = "HH:mm EEE d MMMM yyyy"
+function ordinal_suffix_of(i) {
+    let j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+        return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+        return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+function numberToOrdinal(i) {
+    let j = i % 10,
+        k = i % 100;
+    if (j === 1 && k !== 11) {
+        return i + "st";
+    }
+    if (j === 2 && k !== 12) {
+        return i + "nd";
+    }
+    if (j === 3 && k !== 13) {
+        return i + "rd";
+    }
+    return i + "th";
+}
+
+const authorTimestampFormat = "HH:mm EEE" + numberToOrdinal(d) + " MMMM yyyy"
 
 export function formatAuthorTimestamp(timestamp: string): string {
 	return DateTime.fromISO(
