1557d2e643f3f891b007c73fdde3060ef284e780
Thomas Mills <accounts+github@tomjmills.co.uk>
2024-04-19 14:49:46 +0100

    Apply the same for darkmode and change colours

--- src/routes/HomeHero.svelte
+++ src/routes/HomeHero.svelte
@@ -31,29 +31,11 @@
 				<a href="/about" class="btn variant-filled-primary dark:variant-ghost-surface">Learn More</a>
 			</div>
 		</div>
-		<div class="hidden dark:inline-flex items-center justify-center">
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://www.dow.cam.ac.uk/"
-				class="hidden btn rounded-container-token md:inline-block shadow-2x overflow-hidden cursor-pointer transition-all duration-700"
-			>
-				<svg
-					class="rounded-container-token"
-					viewBox="0 0 1200 1200"
-					fill="rgba(var(--color-primary-500))"
-					width={innerWidth * 0.35}
-					height={innerWidth * 0.35}
-				>
-					<use xlink:href="/favicon.svg#logoNotInverted" />
-				</svg>
-			</a>
-		</div>
 	</div>
 
-	<div class="block h-full md:inline-block dark:hidden z-0">
+	<div class="block h-full md:inline-block z-0">
 		<svg
-			class="bigSvgStyled w-full md:h-auto md:w-[min(55vw,100vh)] absolute left-0 md:left-auto right-0 bottom-0 fill-primary-900/10 md:fill-primary-900 z-0"
+			class="bigSvgStyled w-full md:h-auto md:w-[min(55vw,100vh)] absolute left-0 md:left-auto right-0 bottom-0 fill-primary-900/10 md:fill-primary-900 dark:fill-primary-400/10 md:dark:fill-primary-400 z-0"
 			viewBox="0 0 1200 1200"
 		>
 			<use xlink:href="/favicon.svg#logoInverted" />
