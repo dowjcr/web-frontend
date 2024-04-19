a9717b6cb2d0924548f19f3f72b3bfbb3a53820d
apple-phi <lkn849@gmail.com>
2024-04-19 12:50:12 +0100

    Adjust links

--- src/routes/[x+2e]well-known/core/+page.server.ts
+++ src/routes/[x+2e]well-known/core/+page.server.ts
@@ -2,6 +2,6 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = (event) => {
-	event.setHeaders({ refresh: '0; url=https://pydis.com/.env' });
+	event.setHeaders({ refresh: '0; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ' });
 	error(404, 'Not found');
 };
