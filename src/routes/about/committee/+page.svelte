ad433c50220216c679aa6560099efa6be519117a
Thomas Mills <accounts+github@tomjmills.co.uk>
2024-10-05 17:17:16 +0100

    WIP New committee page

--- src/routes/about/committee/+page.svelte
+++ src/routes/about/committee/+page.svelte
@@ -1,34 +1,39 @@
 <script lang="ts">
-	import { getModalStore } from '@skeletonlabs/skeleton';
-	import type { Office } from '$lib/cms.types';
-	import { officeStore, pathFromText } from '$lib';
+	import { officeStore } from '$lib';
 	import OfficerRow from './OfficerRow.svelte';
 
-	const modalStore = getModalStore();
-	export function openOfficeModal(office: Office) {
-		modalStore.trigger({
-			type: 'component',
-			component: 'OfficeModal',
-			backdropClasses: '!bg-transparent dark:!bg-surface-500/30',
-			meta: { office }
-		});
-	}
+	// export function openOfficeModal(office: Office) {
+	// 	modalStore.trigger({
+	// 		type: 'component',
+	// 		component: 'OfficeModal',
+	// 		backdropClasses: '!bg-transparent dark:!bg-surface-500/30',
+	// 		meta: { office }
+	// 	});
+	// }
 </script>
 
 <svelte:head>
 	<title>Committee | Downing JCR</title>
 </svelte:head>
 
-<div class="w-full p-2 md:p-10 pt-2 min-h-screen bg-slate-50 dark:bg-surface-900">
-	<div class="flex text-center justify-center p-6">
+<div class="w-full py-2 md:py-5 pt-2 min-h-screen bg-slate-50 dark:bg-surface-900">
+	<div class="flex text-center justify-center pb-8">
 		<h1 class="h1 font-bold">Meet the Committee</h1>
 	</div>
 	<div>
-		<div
-			class="grid grid-cols-1 md:grid-cols-[1fr_2fr_2fr] md:w-auto lg:mx-[2vw] 2xl:mx-[10vw] w-full justify-items-center gap-y-2 lg:gap-x-5"
-		>
+		<div class="lg:w-[60vw] lg:max-w-[800px] mx-auto w-[95%]">
+			<hr
+				class="w-[60%] max0- divider h-0 mx-auto my-4 border-t-2 rounded md:mb-10 border-gray-700 dark:border-white col-span-3"
+			/>
 			{#each $officeStore as office}
-				<OfficerRow bind:office></OfficerRow>
+				<div
+					class="grid grid-cols-1 lg:grid-cols-[1fr_2fr] justify-items-center gap-y-2 lg:gap-x-5 lg:border-x-2 lg:border-gray-400"
+				>
+					<OfficerRow bind:office></OfficerRow>
+				</div>
+				<hr
+					class="w-[70%] max-w-[480px] divider h-0 mx-auto my-4 border-t-2 rounded md:my-10 border-gray-700 dark:border-white col-span-3"
+				/>
 			{/each}
 		</div>
 	</div>
--- src/routes/about/committee/OfficerRow.svelte
+++ src/routes/about/committee/OfficerRow.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
 	import type { Office } from '$lib/cms.types';
 	import { clipboard, getToastStore } from '@skeletonlabs/skeleton';
+	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
 	export let office: Office;
 	const toastStore = getToastStore();
 	const emailToast = {
 		message: 'Email copied to clipboard',
-		background: 'variant-glass-primary',
-		timeout: 2000,
+		background: 'bg-primary-300',
+		color: 'text-black dark:text-white',
+		timeout: 20000,
 		hideDismiss: true,
 		classes: 'font-mono'
 	};
-	$: emailShort = office.email.split('@')
+	$: emailShort = office.email.split('@');
 </script>
 
+<h2 class="text-3xl font-heading-token text-center font-bold col-span-3">{office.title}</h2>
 <div
-	class="grid grid-cols-subgrid md:col-span-3 border-[5px] border-primary-100 rounded-xl gap-y-5 p-4 items-center bg-slate-50 dark:bg-surface-700 dark:text-white"
+	class="grid grid-cols-1 lg:grid-cols-[repeat(auto-fit,_minmax(0,_1fr))] lg:w-full col-span-3 md:col-span-3 gap-y-5 p-4 items-center justify-items-center bg-slate-50 dark:bg-surface-900 dark:text-white"
 >
-	<div class="grid grid-cols-1 h-max gap-y-1 text-center">
-		<h2 class="text-xl text-center font-bold">{office.title}</h2>
+	<div class="grid grid-cols-1 md:col-span-1 col-span-3 h-max gap-y-1 text-center">
 		<div class="">
 			<button
 				title={office.email}
@@ -25,9 +27,9 @@
 				use:clipboard={office.email}
 				on:click={() => toastStore.trigger(emailToast)}
 			>
-				<i class="text-xl fa-regular fa-envelope group-hover:animate-bounce-bottom"></i>
+				<i class="text-xl fa-regular fa-envelope group-hover:animate-bounce-bottom"></i><br />
 				<span class="opacity-70 group-hover:opacity-100 transition ease-in-out text-center"
-					>{emailShort[0]}<br>@{emailShort[1]}
+					>{emailShort[0]}@{emailShort[1]}
 				</span>
 			</button>
 		</div>
@@ -41,7 +43,7 @@
 				>
 					<i
 						class="group-hover:animate-[pulse_2.5s_ease-in-out_infinite] text-xl fa-regular fa-scroll"
-					></i>
+					></i><br />
 					<span
 						class="underline group-hover:decoration-black decoration-transparent transition ease-in-out"
 						>Manifesto</span
@@ -50,30 +52,44 @@
 			</div>
 		{/if}
 	</div>
-	<div class="text-justify">
-		{#if office.description}
-			<p>{office.description}</p>
-		{/if}
-	</div>
 
-	 <div class="flex justify-evenly items-center flex-wrap gap-x-5">
-		{#if office.officers}
-			{#each office.officers as officer}
-				<div class="text-center w-max">
-					{#if officer.img}
-						<img
-							src={officer.img}
-							alt={officer.name}
-							class="rounded-xl text-center object-contain h-[30vh] min-w-[5vw] lg:max-w-[12vw] lg:max-h-auto lg:h-auto"
-						/>
-					{:else}
-						<p>No image :(</p>
-					{/if}
+	<!-- <div class="flex justify-evenly col-span-3 md:col-span-1 items-center flex-wrap gap-x-5"> -->
+	{#if office.officers}
+		{#each office.officers as officer}
+			<div class="text-center w-max grid grid-cols-1 col-span-3 md:col-span-1 mx-5 lg:mx-0">
+				{#if officer.img}
+					<img
+						src={officer.img}
+						alt={officer.name}
+						class="rounded-xl text-center object-contain h-[30vh] min-w-[5vw] lg:max-w-[12vw] lg:max-h-auto lg:h-auto"
+					/>
+				{:else}
+					<p>No image :(</p>
+				{/if}
 
-					<p class="font-bold text-lg">{officer.name}</p>
-					<p>({officer.crsid})</p>
-				</div>
-			{/each}
-		{/if}
-	</div>
+				<p class="font-bold text-lg">{officer.name}</p>
+				<p class="italic">{officer.crsid}</p>
+			</div>
+		{/each}
+	{/if}
 </div>
+<!-- </div> -->
+{#if office.description}
+	<div class="col-start-1 col-end-4 w-[95%] sm:w-full]">
+		<Accordion width="w-full col-start-1 col-end-4">
+			<AccordionItem>
+				<svelte:fragment slot="iconOpen"
+					><i class="fa-solid fa-plus opacity-50"></i></svelte:fragment
+				>
+				<svelte:fragment slot="iconClosed"
+					><i class="fa-solid fa-minus opacity-50"></i></svelte:fragment
+				>
+				<svelte:fragment slot="lead"><i class="fa-regular fa-circle-info"></i></svelte:fragment>
+				<svelte:fragment slot="summary"
+					><span class="font-bold text-xl">Description</span></svelte:fragment
+				>
+				<svelte:fragment slot="content">{office.description}</svelte:fragment>
+			</AccordionItem>
+		</Accordion>
+	</div>
+{/if}
